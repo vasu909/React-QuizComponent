e0413cf667fd91578161890f8a0bdfd4346610e6
vasu909 <vasu909@gmail.com>
2022-08-17 13:23:25 +0530

    Update QuizQuestionButton.js

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import QuizQuestionButton from './QuizQuestionButton.js'
 
-class QuizQuestionButton extends Component {
+class QuizQuestion extends Component {
+  handleClick(buttonText) {
+    if(buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler()
+    }
+  }
   render() {
     return (
-      <li>
-        <button>{this.props.button_text}</button>
-      </li>
+      <main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            {this.props.quiz_question.answer_options.map((answer_option, index) => {
+              return <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />
+            })}
+          </ul>
+        </section>
+      </main>      
     )
   }
 }
 
-export default QuizQuestionButton
+export default QuizQuestion
